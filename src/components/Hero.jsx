4ed07c0799a2360b672bdab6e77361b53bf7b502
sharkiiee <sarthak231203@gmail.com>
2026-01-19 21:23:29 +0530

    fixed with of the her

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ import { UserRoundCog } from "lucide-react";
 export default function Hero() {
     const asideDivStyle = "flex items-center gap-2 text-lg cursor-pointer hover:bg-blue-800 px-3 py-2 rounded-md w-full rounded transition-colors";
   return (
-    <div>
-      <aside className="border bg-blue-950 text-white h-screen flex w-80 flex-col gap-10 items-start px-5 pt-10 rounded-r-xl">
+    <div className="h-[calc(100vh-6rem)] border bg-gray-100 flex">
+      <aside className="border bg-blue-950 text-white h-full flex w-80 flex-col gap-5 items-start px-5 pt-10 rounded-r-xl">
         <div className={asideDivStyle}>
           <LayoutDashboard size={37} className=" p-2 rounded-lg" />
           <p>Dashboard</p>
